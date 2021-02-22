37f0496127e88219505eeb23aa84d57b3e14f25c
Valery Akimov <valery.e.akimov@gmail.com>
2021-02-22 17:11:39 +0300

    Add: practice 13

---
+++ practice13/src/main/java/ru/mirea/models/Student.java
@@ -0,0 +1,25 @@
+package ru.mirea.models;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Data
+@ToString
+@Component
+public class Student {
+    @Value("${student.name}")
+    private String name;
+    @Value("${student.last_name}")
+    private String lastName;
+    @Value("${student.group}")
+    private String group;
+
+    @PostConstruct
+    private void init() {
+        System.out.println(this);
+    }
+}
