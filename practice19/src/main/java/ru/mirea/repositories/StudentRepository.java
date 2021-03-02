43a6ec3e7a478affe7897593bb7f775148471792
Valery Akimov <valery.e.akimov@gmail.com>
2021-03-02 17:04:15 +0300

    Add: practice 19

---
+++ practice19/src/main/java/ru/mirea/repositories/StudentRepository.java
@@ -0,0 +1,11 @@
+package ru.mirea.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.mirea.models.Group;
+import ru.mirea.models.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>, StudentFilter {
+    void deleteAllByGroup(Group group);
+}
